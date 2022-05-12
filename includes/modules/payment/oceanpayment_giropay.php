b2d42c0783206d2a80a7976bb356cc8361dfcfdd
Oceanpayment <102337499+Oceanpayment@users.noreply.github.com>
2022-05-12 16:54:36 +0800

    update setting

--- includes/modules/payment/oceanpayment_giropay.php
+++ includes/modules/payment/oceanpayment_giropay.php
@@ -391,7 +391,9 @@ class oceanpayment_giropay{
 			}
 		}
 		
-		$this->returnLog($logtype);
+		if(MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_LOGS == 'True'){
+		    $this->returnLog($logtype);
+		}
 		
 		$comments = $logtype . '(' .$comments . ')' . 'payment_id: ' . $payment_id . ' | order_number: ' . $order_number . ' | ' . $order_currency . ':' . $order_amount . ' | payment_details: ' . $payment_details;
 		$sql_data_array = array('orders_id'         => (int)$order_number,
@@ -555,7 +557,9 @@ class oceanpayment_giropay{
 		$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('" . MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_TEXT_CONFIG_12_1 . "', 'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_HANDLER', 'https://secure.oceanpayment.com/gateway/service/test', '" . MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_TEXT_CONFIG_12_2 . "', '6', '20', '', now())");
 		//在数据库中插入模块设置(响应代码)
 		$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('" . MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_TEXT_CONFIG_14_1 . "', 'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_RESPONSE_CODE', 'false', '" . MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_TEXT_CONFIG_14_2 . "', '6', '22', 'zen_cfg_select_option(array(\'Online\', \'Local\'), ', now())");
-		
+		//在数据库中插入模块设置（是否写日志）
+        	$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('" . MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_TEXT_CONFIG_19_1 . "', 'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_LOGS', 'True', '" . MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_TEXT_CONFIG_19_2 . "', '6', '2', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
+
 	}
 	
 	
@@ -579,6 +583,7 @@ class oceanpayment_giropay{
 				'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_SORT_ORDER',
 				'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_HANDLER',
 				'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_RESPONSE_CODE',
+                		'MODULE_PAYMENT_OCEANPAYMENT_GIROPAY_LOGS',
 		);	
 	}
 	
