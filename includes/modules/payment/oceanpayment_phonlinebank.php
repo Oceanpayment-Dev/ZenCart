44564f28a47c23ca3795b4e702e2440fbecc6169
Oceanpayment <102337499+Oceanpayment@users.noreply.github.com>
2022-05-12 17:06:40 +0800

    update setting

--- includes/modules/payment/oceanpayment_phonlinebank.php
+++ includes/modules/payment/oceanpayment_phonlinebank.php
@@ -419,8 +419,9 @@ class oceanpayment_phonlinebank{
 			
 		}
 		
-		
-		$this->returnLog($logtype);
+		if(MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_LOGS == 'True'){
+		    $this->returnLog($logtype);
+		}
 		
 		$comments = $logtype . '(' .$comments . ')' . 'payment_id: ' . $payment_id . ' | order_number: ' . $order_number . ' | ' . $order_currency . ':' . $order_amount . ' | payment_details: ' . $payment_details;
 		$sql_data_array = array('orders_id'         => (int)$order_number,
@@ -617,7 +618,9 @@ class oceanpayment_phonlinebank{
 		$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('" . MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_TEXT_CONFIG_12_1 . "', 'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_HANDLER', 'https://secure.oceanpayment.com/gateway/service/test', '" . MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_TEXT_CONFIG_12_2 . "', '6', '20', '', now())");
 		//在数据库中插入模块设置(响应代码)
 		$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('" . MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_TEXT_CONFIG_14_1 . "', 'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_RESPONSE_CODE', 'false', '" . MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_TEXT_CONFIG_14_2 . "', '6', '22', 'zen_cfg_select_option(array(\'Online\', \'Local\'), ', now())");
-		
+		//在数据库中插入模块设置（是否写日志）
+        	$db->Execute("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('" . MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_TEXT_CONFIG_19_1 . "', 'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_LOGS', 'True', '" . MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_TEXT_CONFIG_19_2 . "', '6', '2', 'zen_cfg_select_option(array(\'True\', \'False\'), ', now())");
+
 		
 	}
 	
@@ -642,6 +645,7 @@ class oceanpayment_phonlinebank{
 				'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_SORT_ORDER',
 				'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_HANDLER',
 				'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_RESPONSE_CODE',
+                		'MODULE_PAYMENT_OCEANPAYMENT_PHONLINEBANK_LOGS',
 		);	
 	}
 	
